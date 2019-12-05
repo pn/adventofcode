216a50782754528fa18a48dfc1d11873491131dd
Paweł Nadolski <pawel.nadolski@gmail.com>
2019-12-05 09:56:36 +0100

    Refactor

--- day5.py
+++ day5.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
 f = open('input5.txt')
 _in = 1
-line = f.readline()
-a = [int(x) for x in line.split(',')]
-i = 0
+a = [int(x) for x in f.readline().split(',')]
 def read(ar, val, mode):
     assert(0 <= mode <= 1)
     if mode == 0:
         return ar[val]
     elif mode == 1:
         return val
-
+i = 0
 while a[i] != 99:
-    op = a[i] % 100
-    b = a[i] // 100
-    mode1 = b % 10
-    b = b // 10
-    mode2 = b % 10
-    b = b // 10
-    mode3 = b % 10
-    b = b // 10
-    assert(mode3 == 0)
-        
+    op, b = a[i] % 100, a[i] // 100
+    mode1, b = b % 10, b // 10
+    mode2, b = b % 10, b // 10
+
     if op == 1:
         a[a[i+3]] = read(a, a[i+1], mode1) + read(a, a[i+2], mode2)
         i += 4
@@ -34,5 +26,3 @@ while a[i] != 99:
     elif op == 4:
         print(read(a, a[i+1], mode1))
         i += 2
-    else:
-        raise ValueError()
