849379dbb54785e8e2012887d9437897668faedf
Paweł Nadolski <pawel.nadolski@gmail.com>
2020-12-02 06:55:22 +0100

    Refactor day 2 solution

--- 2020/day2p1.py
+++ 2020/day2p1.py
@@ -1,10 +1,8 @@
 valid = 0
 for line in open('input2.txt'):
-    parts = line.split(' ')
-    l_min = int(parts[0].split('-')[0])
-    l_max = int(parts[0].split('-')[1])
-    letter = parts[1][0]
-    password = parts[2].strip('\n')
+    parts = line.strip('\n').split(' ')
+    l_min, l_max = [int(part) for part in parts[0].split('-')]
+    letter, password = parts[1][0], parts[2]
     if password.count(letter) in range(l_min, l_max + 1):
         valid += 1
 print(valid)
--- 2020/day2p2.py
+++ 2020/day2p2.py
@@ -1,10 +1,8 @@
 valid = 0
 for line in open('input2.txt'):
-    parts = line.split(' ')
-    first = int(parts[0].split('-')[0])
-    second = int(parts[0].split('-')[1])
-    letter = parts[1][0]
-    password = parts[2].strip('\n')
+    parts = line.strip('\n').split(' ')
+    first, second = [int(part) for part in parts[0].split('-')]
+    letter, password = parts[1][0], parts[2]
     if (password[first-1] == letter) != (password[second-1] == letter):
         valid += 1
 print(valid)
