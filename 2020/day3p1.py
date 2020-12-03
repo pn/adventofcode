cacf41448b9a484e822869142d5b5f61915d3ba2
Paweł Nadolski <pawel.nadolski@gmail.com>
2020-12-03 09:30:43 +0100

    Add alternative solutions for day 3

--- 2020/day3p1.py
+++ 2020/day3p1.py
@@ -4,3 +4,12 @@ for line in open('input3.txt').read().split('\n'):
         trees += 1
     position = (position + 3) % len(line)
 print(trees)
+
+# alternative solution
+
+lines = open('input3.txt').read().split('\n')
+print([line[(i * 3) % len(line)] for i, line in enumerate(lines)].count('#'))
+
+# one-liner
+
+print([l[(i * 3) % len(l)] for i, l in enumerate(open('input3.txt').read().split('\n'))].count('#'))
--- 2020/day3p2.py
+++ 2020/day3p2.py
@@ -4,8 +4,30 @@ result, width = 1, len(lines[0])
 for slope in slopes:
     trees = position = 0
     for i in range(0, len(lines), slope[1]):
-        if lines[i][position] == '#':
+        if lines[i][position % width] == '#':
             trees += 1
         position = (position + slope[0]) % width
     result *= trees
+print(result)
+
+# alternative solution
+
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+lines = open('input3.txt').read().split('\n')
+result, width = 1, len(lines[0])
+for slope in slopes:
+    trees = 0
+    for i in range(0, len(lines), slope[1]):
+        if lines[i][(i // slope[1] * slope[0]) % width] == '#':
+            trees += 1
+    result *= trees
+print(result)
+
+# alternative solution
+
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+lines = open('input3.txt').read().split('\n')
+result = 1
+for slope in slopes:
+    result *= [l[(i // slope[1] * slope[0]) % len(l)] for i, l in enumerate(lines) if (i % slope[1]) == 0].count('#')
 print(result)
