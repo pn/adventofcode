68243d6b5ca49daef86495fff718996f8d2a239a
Paweł Nadolski <pawel.nadolski@gmail.com>
2020-12-04 09:49:43 +0100

    Refactor

--- 2020/day4p2.py
+++ 2020/day4p2.py
@@ -11,7 +11,7 @@ for line in open('input4.txt').read().splitlines():
                 field == 'eyr' and 2020 <= int(value) <= 2030 or \
                 field == 'hgt' and (value.endswith('cm') and 150 <= int(value.rstrip('cm')) <= 193 or
                                     value.endswith('in') and 59 <= int(value.rstrip('in')) <= 76) or \
-                field == 'hcl' and re.match(r'^#[0-9a-z]{6}$', value) or \
+                field == 'hcl' and re.match(r'^#[0-f]{6}$', value) or \
                 field == 'ecl' and value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'] or \
                 field == 'pid' and re.match(r'^[0-9]{9}$', value):
             passports[-1].append(field)
