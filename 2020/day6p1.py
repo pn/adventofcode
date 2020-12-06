c2a6974290d55af9df673e8b4bca9c021c81bf75
Paweł Nadolski <pawel.nadolski@gmail.com>
2020-12-06 07:05:03 +0100

    Add alternative solutions for day 6

--- 2020/day6p1.py
+++ 2020/day6p1.py
@@ -6,5 +6,9 @@ for line in open('input6.txt').read().splitlines():
         groups.append([])
         continue
     groups[-1].append(set(list(line)))
-
 print(sum(len(reduce(lambda a, b: a.union(b), group)) for group in groups))
+
+# alternative solution
+
+groups = [(list(set(list(ans)) for ans in group.split('\n'))) for group in open('input6.txt').read().split('\n\n')]
+print(sum(len(group[0].union(*group[1:])) for group in groups))
--- 2020/day6p2.py
+++ 2020/day6p2.py
@@ -8,3 +8,8 @@ for line in open('input6.txt').read().splitlines():
     groups[-1].append(set(list(line)))
 
 print(sum(len(reduce(lambda a, b: a.intersection(b), group)) for group in groups))
+
+# alternative solution
+
+groups = [[set(list(answers)) for answers in group.split('\n')] for group in open('input6.txt').read().split('\n\n')]
+print(sum(len(group[0].intersection(*group[1:])) for group in groups))
